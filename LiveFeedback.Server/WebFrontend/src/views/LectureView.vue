d62b14818ec736b8afacda77a9257c30c97df9a6
BeautifulBeach <214304905+BeautifulBeach@users.noreply.github.com>
2025-07-20 01:37:44 +0200

    Reimplemented styling for JS based frontend

--- LiveFeedback.Server/WebFrontend/src/views/LectureView.vue
+++ LiveFeedback.Server/WebFrontend/src/views/LectureView.vue
@@ -5,14 +5,65 @@ import { signalRService } from '@/services/signalRService.ts'
 const currentRating: Ref<number> = ref(50)
 
 watch(currentRating, async (newRating) => {
-  await signalRService.sendNewRating(newRating)
+  await signalRService.sendNewRating(Math.round(newRating))
 })
 </script>
 
 <template>
-  <input type="range" v-model.number="currentRating" />
+  <div class="slider-container">
+    <input type="range" v-model.number="currentRating" class="slider" step="0.01" />
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+@mixin slider-track {
+  height: 24px;
+  border-radius: 1rem;
+  background: linear-gradient(90deg, var(--lf-blue), var(--lf-turquoise), var(--lf-green), var(--lf-yellow), var(--lf-red));
+  border: none;
+}
 
+@mixin slider-thumb {
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 4px rgba(0, 0, 0, .2);
+  appearance: none;
+  margin-top: -12px;
+}
+
+.slider-container {
+  width: 100vw;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.slider {
+  width: 90%;
+  max-width: 1200px;
+  background: none;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  &::-webkit-slider-runnable-track {
+    @include slider-track;
+  }
+
+  &::-webkit-slider-thumb {
+    @include slider-thumb;
+  }
+
+  &::-moz-range-track {
+    @include slider-track;
+  }
+
+  &::-moz-range-thumb {
+    @include slider-thumb;
+  }
+}
 </style>
+
